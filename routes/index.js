3242a2d80285e4c7d30426eed39deebdd58175f4
Ivan Tarasov <guser999@mail.ru>
2021-12-18 22:11:18 +0300

    refactor(code) Мелкие правки

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 
-router.post('/data', function(req, res, next) {
-    res.render('result', { title: 'Круги Гаджиева', moreDatas: req.body.tasks.reverse() });
+router.post('/data', function (req, res, next) {
+  res.render('result', {title: 'Круги Гаджиева', moreDatas: req.body.tasks.reverse()});
 });
 
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Круги Гаджиева' });
+router.get('/', function (req, res, next) {
+  res.render('index', {title: 'Круги Гаджиева'});
 });
 
 
